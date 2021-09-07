836f88377057aa70de252536c6a37e918c13ae2c
Brad Bell <bradbell@seanet.com>
2021-09-07 05:54:16 -0700

    master: first version the builds its own documentaiton. Copy files from cppad_py.git repository. The following files were modified for this repository: run_sphinx.sh, conf.py, spelling.

---
+++ bin/run_sphinx.sh
@@ -0,0 +1,74 @@
+#! /bin/bash -e
+# -----------------------------------------------------------------------------
+#                      xsrst: Extract Sphinx RST Files
+#          Copyright (C) 2017-21 Bradley M. Bell (bradbell@seanet.com)
+#              This program is distributed under the terms of the
+#              GNU General Public License version 3.0 or later see
+#                    https://www.gnu.org/licenses/gpl-3.0.txt
+# -----------------------------------------------------------------------------
+# bash function that echos and executes a command
+echo_eval() {
+    echo $*
+    eval $*
+}
+# -----------------------------------------------------------------------------
+if [ "$0" != 'bin/run_sphinx.sh' ]
+then
+    echo 'must execut bin/run_sphinx.sh from its parent directory'
+    exit 1
+fi
+# -----------------------------------------------------------------------------
+if [ "$1" != 'html' ] && [ "$1" != 'pdf' ]
+then
+    echo 'usage: bin/run_sphinx (html|pdf)'
+    exit 1
+fi
+target="$1"
+# -----------------------------------------------------------------------------
+# preamble
+preamble='sphinx/preamble.rst'
+# -----------------------------------------------------------------------------
+if ! grep BEGIN_LATEX_MACROS $preamble > /dev/null
+then
+    echo "bin/run_sphinx: can't find BEGIN_LATEX_MACROS in $premable"
+fi
+if ! grep END_LATEX_MACROS $preamble > /dev/null
+then
+    echo "bin/run_sphinx: can't find END_LATEX_MACROS in $premable"
+fi
+# -----------------------------------------------------------------------------
+# xsrst
+echo_eval bin/xsrst.py $target bin/xsrst.py sphinx spelling keyword
+# -----------------------------------------------------------------------------
+# html
+# -----------------------------------------------------------------------------
+if [ "$target" == 'html' ]
+then
+    sphinx-build -b html sphinx doc/html
+    echo 'run_sphinx.sh: OK'
+    exit 0
+fi
+# -----------------------------------------------------------------------------
+# pdf
+# -----------------------------------------------------------------------------
+#
+diff=$(git diff $preamble)
+if [ "$diff" != '' ]
+then
+    echo 'bin/run_sphinx.sh pdf:'
+    echo "$preamble has changed."
+    echo 'You must first test bin/run_sphinx.sh html.'
+    echo "Then check in the new $preamble before running bin/run_sphinx.sh pdf."
+    exit 1
+fi
+echo "sed -i $preamble -e '/BEGIN_LATEX_MACROS/,/END_LATEX_MACROS/d'"
+sed -i $preamble -e '/BEGIN_LATEX_MACROS/,/END_LATEX_MACROS/d'
+echo_eval sphinx-build -b latex sphinx doc/latex
+echo_eval git checkout $preamble
+echo_eval cd doc/latex
+echo "sed -i xsrst.tex -e 's|\\chapter{|\\section{|'"
+sed -i xsrst.tex -e 's|\\chapter{|\\section{|'
+echo_eval make xsrst.pdf
+# -----------------------------------------------------------------------------
+echo 'run_sphinx.sh: OK'
+exit 0
---
+++ sphinx/test_in/children.py
@@ -0,0 +1,42 @@
+# -----------------------------------------------------------------------------
+#         cppad_py: A C++ Object Library and Python Interface to Cppad
+#          Copyright (C) 2017-20 Bradley M. Bell (bradbell@seanet.com)
+#              This program is distributed under the terms of the
+#              GNU General Public License version 3.0 or later see
+#                    https://www.gnu.org/licenses/gpl-3.0.txt
+# ----------------------------------------------------------------------------
+# BEGIN_FILE
+"""
+{xsrst_begin children_exam}
+
+Children Example
+################
+This file does not contain a begin parent command,
+so all its sections are children of the section that includes it.
+
+Other Section
+*************
+The :ref:`link<children_other>` goes to the other section in this file.
+
+This File
+*********
+{xsrst_file
+    # BEGIN_FILE
+    # END_FILE
+}
+
+{xsrst_end children_exam}
+"""
+# ----------------------------------------------------------------------------
+"""
+{xsrst_begin children_other}
+
+Other Child
+###########
+This is the other child section in
+:ref:`this file<children_exam.this_file>`.
+
+
+{xsrst_end children_other}
+"""
+# END_FILE
