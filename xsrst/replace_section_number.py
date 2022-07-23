e5ce663a1ccb0799de6eec4ed952adb6095299ce
Brad Bell <bradbell@seanet.com>
2022-07-23 04:59:54 -0700

    master: fix replce_section_number.py when section_number is non-empty.

--- xsrst/replace_section_number.py
+++ xsrst/replace_section_number.py
@@ -8,18 +8,22 @@
 # Replace {xsrst_section_number} by it section number or empty string.
 #
 # data_in:
-# data before replacement. This must contain {xsrst_section_number}
-# at the end of a line. Referred to as the command. There must be a
-# section title after the command. The section tittle may have an rst
-# overline directly before it and must have an underline directly after it.
+# data before replacement. This must contain \n{xsrst_section_number}
+# which is referred to as the command below. There must be a
+# section title after the command (starting with a newline).
+# This section title may have an rst overline directly before the heading text
+# and must have an underline directly after it.
 # If both an overline and underline follow, they must be equal.
 #
 # section_number:
-# The replacement for the command. This may be empty;
-# i.e., the replacement text is the empty string.
+# This is a section number that is placed infront of the heading text.
+# This may be empty; i.e., the replacement text is the empty string.
+# The underline (and overline if present) are exended to by the number of
+# characters added to the heading text.
 #
 # data_out:
-# the return data_out is the data after replacement. This is a python str
+# the return data_out is the data after replacement. The section number is
+# added (see above) and he command is removed.
 #
 # data_out =
 def replace_section_number(data_in, section_number) :
@@ -33,25 +37,26 @@ def replace_section_number(data_in, section_number) :
         return data_in.replace(pattern,'')
     #
     # punctuation
-    # Headings used repeated copies of one of these characters
+    # Headings uses repeated copies of one of these characters
     punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
     #
     # start_cmd
     start_cmd = data_in.find(pattern)
     assert 0 <= start_cmd
     #
-    # check first character after command
-    ch  = data_in[start_cmd + len(pattern)]
-    assert ch == '\n'
+    # first_newline
+    first_newline = start_cmd + len(pattern)
+    assert data_in[first_newline] == '\n'
     #
-    # second_index, third_index
-    # index of second and third newline after command
-    second_index = data_in.index('\n', start_cmd + len(pattern) + 1)
-    third_index  = data_in.index('\n', second_index + 1)
+    # second_newline
+    second_newline = data_in.index('\n', first_newline + 1)
+    #
+    # third_newline
+    third_newline  = data_in.index('\n', second_newline + 1)
     #
     # first_line, second_line
-    first_line   = data_in[start_cmd + len(pattern) + 1 : second_index ]
-    second_line  = data_in[ second_index + 1 : third_index ]
+    first_line   = data_in[first_newline + 1 : second_newline ]
+    second_line  = data_in[second_newline + 1 : third_newline ]
     #
     # overline
     # check for an overline directly after the command
@@ -61,7 +66,7 @@ def replace_section_number(data_in, section_number) :
             overline = True
     #
     if not overline :
-        # If no overline so the section title followed the command and then
+        # If no overline the section title follows the command and then
         # an underline after the title.
         assert second_line[0] in punctuation
         #
@@ -71,11 +76,11 @@ def replace_section_number(data_in, section_number) :
             second_line += second_line[0] * ( len(section_number) + 1 )
         #
     else :
-        # fourth_index
-        fourth_index = data_in.index('\n', third_index + 1)
+        # fourth_newline
+        fourth_newline = data_in.index('\n', third_newline + 1)
         #
         # third_line
-        third_line   = data_in[third_index + 1 : fourth_index]
+        third_line   = data_in[third_newline + 1 : fourth_newline]
         assert first_line == third_line
         #
         # new version of first, second, and third lines
@@ -89,7 +94,9 @@ def replace_section_number(data_in, section_number) :
     data_out += first_line + '\n'
     data_out += second_line + '\n'
     if overline :
-        data_out += third_line
-    data_out += data_in[third_index :]
+        data_out += third_line + '\n'
+        data_out += data_in[fourth_newline :]
+    else :
+        data_out += data_in[third_newline :]
     #
     return data_out
