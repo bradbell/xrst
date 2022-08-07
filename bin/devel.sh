ac0a0c680e9310de7b507ba4a3d939947427bd3d
Brad Bell <bradbell@seanet.com>
2022-08-07 05:52:49 -0700

    master: advance to version 2022.8.07

--- bin/devel.sh
+++ bin/devel.sh
@@ -12,6 +12,7 @@ image_link='https://bradbell.github.io/'
 # List of files, besides CMakeLists.txt, that have have a copy of the
 # version number (can be empty).
 version_files='
+    doc.xrst
 '
 #
 # List of special files, for this repository, that the devel tools ignore.
