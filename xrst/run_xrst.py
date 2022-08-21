b081c78ce4c833707d1d07b0a2e499c577ad50d7
Brad Bell <bradbell@seanet.com>
2022-08-20 20:11:24 -0700

    master: Advance to xrst-2022.8.20

--- xrst/run_xrst.py
+++ xrst/run_xrst.py
@@ -236,7 +236,7 @@ import xrst
 # version
 # The script that updates version nubmers expects version at begining of line
 # and to have the value surrounded by single quotes.
-version = '2022.8.15'
+version = '2022.8.20'
 #
 def run_xrst() :
     #
