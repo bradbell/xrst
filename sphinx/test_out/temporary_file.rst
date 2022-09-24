039c02b0a8ee49f06b154ad27b43f4b6970ae8c2
Brad Bell <bradbell@seanet.com>
2022-09-24 05:31:48 -0700

    master: dev documentation: add temporary_file.

---
+++ sphinx/test_out/temporary_file.rst
@@ -0,0 +1,107 @@
+.. include:: xrst_preamble.rst
+
+.. _temporary_file:
+
+!!!!!!!!!!!!!!
+temporary_file
+!!!!!!!!!!!!!!
+
+xrst input file: ``xrst/temporary_file.py``
+
+.. meta::
+   :keywords: temporary_file, write, temporary, rst, page
+
+.. index:: temporary_file, write, temporary, rst, page
+
+.. _temporary_file-0:
+
+Write the temporary RST file for a page
+#######################################
+.. contents::
+   :local:
+
+.. meta::
+   :keywords: rst_line
+
+.. index:: rst_line
+
+.. _temporary_file@rst_line:
+
+rst_line
+********
+is an int version of the :ref:`run_xrst@rst_line`
+argument to the xrst program (with None represented by zero).
+
+.. meta::
+   :keywords: pseudo_heading
+
+.. index:: pseudo_heading
+
+.. _temporary_file@pseudo_heading:
+
+pseudo_heading
+**************
+is the :ref:`process_headings@Returns@pseudo_heading` for this page.
+It is placed before all the other headings in this page.
+A label is added just before the pseudo heading that
+links to it using the page name.
+
+.. meta::
+   :keywords: file_in
+
+.. index:: file_in
+
+.. _temporary_file@file_in:
+
+file_in
+*******
+is the name of the xrst input file for this page.
+
+.. meta::
+   :keywords: tmp_dir
+
+.. index:: tmp_dir
+
+.. _temporary_file@tmp_dir:
+
+tmp_dir
+*******
+is the directory where the output file will be saved.
+
+.. meta::
+   :keywords: page_name
+
+.. index:: page_name
+
+.. _temporary_file@page_name:
+
+page_name
+*********
+is the name of this page.  The output file is tmp_dir/page_name.rst.
+
+.. meta::
+   :keywords: data_in
+
+.. index:: data_in
+
+.. _temporary_file@data_in:
+
+data_in
+*******
+is the data for this page with all the xrst commands converted to
+their sphinx RST values, except the ``{`` xrst_page_number ``}`` command.
+The following is added to this data before writing it to the output file:
+
+#. The preamble is included at the beginning.
+#. The pseudo heading and its label are added next.
+#. The name of the input file file_in is displayed next.
+#. More than 2 lines with only tabs or space are converted to 2 empty lines.
+#. Empty lines at the end are removed
+#. The line numbers are removed.
+#. The text ``\{xrst_`` is replaced by ``{xrst_`` .
+#. if rst_line > 0, a mapping from RST line numbers to file_in line numbers
+   is included at the end.
+
+.. literalinclude:: ../../xrst/temporary_file.py
+   :lines: 61-73
+   :language: py
--- xrst/temporary_file.py
+++ xrst/temporary_file.py
@@ -6,42 +6,58 @@ import xrst
 pattern_line_number = re.compile( r'\n[ \t]*{xrst_line [0-9]+@' )
 pattern_newline_3   = re.compile( r'(\n[ \t]*){2,}\n' )
 # ----------------------------------------------------------------------------
-# Write the temporary RST file for a page.
+# {xrst_begin temporary_file dev}
+# {xrst_spell
+#  tmp
+#  dir
+#  file file
+# }
+# {xrst_comment_ch #}
 #
-# rst_line:
-# is an int version of the rst_line arguemnt to the xrst program
-# (with None replaced by zero).
+# Write the temporary RST file for a page
+# #######################################
 #
-# pseudo_heading:
-# is the pseudoc heading for this page. This is the page name
-# surrounded by headding indicators. `It is placed before
-# all the other headings in this page.
+# rst_line
+# ********
+# is an int version of the :ref:`run_xrst@rst_line`
+# argument to the xrst program (with None represented by zero).
+#
+# pseudo_heading
+# **************
+# is the :ref:`process_headings@Returns@pseudo_heading` for this page.
+# It is placed before all the other headings in this page.
 # A label is added just before the pseudo heading that
 # links to it using the page name.
 #
-# file_in:
+# file_in
+# *******
 # is the name of the xrst input file for this page.
 #
 # tmp_dir
+# *******
 # is the directory where the output file will be saved.
 #
 # page_name
+# *********
 # is the name of this page.  The output file is tmp_dir/page_name.rst.
 #
 # data_in
-# is the data for this page with all the xrst commands coverted to
-# their sphinx RST values, except the {xrst_page_number} command.
-# The following is added to this deta before writing it to the output file:
+# *******
+# is the data for this page with all the xrst commands converted to
+# their sphinx RST values, except the ``{`` xrst_page_number ``}`` command.
+# The following is added to this data before writing it to the output file:
+#
 # #. The preamble is included at the beginning.
 # #. The pseudo heading and its label are added next.
-# #. The name of the input file file_in is dispalyed next.
+# #. The name of the input file file_in is displayed next.
 # #. More than 2 lines with only tabs or space are converted to 2 empty lines.
 # #. Empty lines at the end are removed
 # #. The line numbers are removed.
-# #. The text \{xrst_ is replaced by {xrst_.
-# #. if error_lne > 0, a mapping from RST line numbers to file_in line numbers
+# #. The text ``\\{xrst_`` is replaced by ``\{xrst_`` .
+# #. if rst_line > 0, a mapping from RST line numbers to file_in line numbers
 #    is included at the end.
 #
+# {xrst_code py}
 def temporary_file(
    rst_line,
    pseudo_heading,
@@ -55,6 +71,8 @@ def temporary_file(
    assert type(file_in) == str
    assert type(page_name) == str
    assert type(data_in) == str
+   # {xrst_code}
+   # {xrst_end temporary_file}
    #
    # label
    # label that displays page name (which is text in pseudo heading)
