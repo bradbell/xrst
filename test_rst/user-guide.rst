1a11df5f92f61dfb069476176604f36a072bcc6d
Brad Bell <bradbell@seanet.com>
2023-12-18 12:30:16 -0700

    master: Advance to xrst-2023.12.18

--- test_rst/user-guide.rst
+++ test_rst/user-guide.rst
@@ -40,7 +40,7 @@ Git Repository
 
 Version of this Documentation
 *****************************
-xrst-2023.12.17
+xrst-2023.12.18
 
 .. meta::
    :keywords: install, stable, version
--- xrst/run_xrst.py
+++ xrst/run_xrst.py
@@ -533,7 +533,7 @@ import xrst
 # version
 # The script that updates version numbers expects version at begining of line
 # and to have the value surrounded by single quotes.
-version = '2023.12.17'
+version = '2023.12.18'
 #
 def run_xrst() :
    #
