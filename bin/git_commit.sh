cf96d3d3c3b42dd531257cb49b7fd87c7dbd712b
Brad Bell <bradbell@seanet.com>
2024-03-13 08:52:29 -0700

    master: git_commit.sh: add instructions for retrieving the previous message.

--- bin/git_commit.sh
+++ bin/git_commit.sh
@@ -63,31 +63,35 @@ do
    fi
 done
 # -----------------------------------------------------------------------------
-# git_commit.log
+# temp.log
 branch=$(git branch --show-current)
-cat << EOF > git_commit.log
+cat << EOF > temp.log
 $branch:
-# Enter the commit message for your changes above.
-# This commit will abort if the first line does not begin with "$branch:"
-# because $branch is the branch for this commit.
-# Lines starting with '#' are not included in the message.
-# Below is a list of the files for this commit:
+# 1. Enter message for this commit above this line. 
+# 2. The message for the previous commit is in git_commit.log (if it exists).
+# 3. This commit will abort if the first line does not begin with "$branch:"
+#    because $branch is the branch for this commit.
+# 4. Lines starting with '#' are not included in the message.
+# 5. Below is a list of the files for this commit:
 EOF
-git status --porcelain | sed -e 's|^|# |' >> git_commit.log
-$EDITOR git_commit.log
-sed -i git_commit.log -e '/^#/d'
-if ! head -1 git_commit.log | grep "^$branch:" > /dev/null
+git status --porcelain | sed -e 's|^|# |' >> temp.log
+$EDITOR temp.log
+sed -i temp.log -e '/^#/d'
+if ! head -1 temp.log | grep "^$branch:" > /dev/null
 then
    echo "Aborting because first line does not begin with $branch:"
-   echo 'See ./git_commit.log'
+   echo 'See ./temp.log'
    exit 1
 fi
-if ! head -1 git_commit.log | grep "^$branch:.*[^ \t]" > /dev/null
+if ! head -1 temp.log | grep "^$branch:.*[^ \t]" > /dev/null
 then
    echo "Aborting because only white space follow $branch: in first line"
-   echo 'See ./git_commit.log'
+   echo 'See ./temp.log'
    exit 1
 fi
+#
+# git_commit.log
+mv temp.log git_commit.log
 # -----------------------------------------------------------------------------
 # git add
 echo_eval git add --all
