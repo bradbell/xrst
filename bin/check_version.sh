fe3571ffdcb2cc967d3d4a6ab2d70fe3798ba2a2
Brad Bell <bradbell@seanet.com>
2025-01-09 18:45:29 -0700

    master: check_version.sh: fix setting yyyy.month.day.year format. user.xrst: put this version number in documentation.

--- bin/check_version.sh
+++ bin/check_version.sh
@@ -118,7 +118,7 @@ s|(["'])[0-9]{8}[.][0-9]{1,2}(["'])|\\1$version\\2|
 s|(["'])[0-9]{4}[.][0-9]{1,2}[.][0-9]{1,2}(["'])|\\1$version\\2|
 s|$package_name-[0-9]{8}|$package_name-$version|
 s|$package_name-[0-9]{8}[.][0-9]{1,2}|$package_name-$version|
-s|$package_name-[0-9]{8}[.][0-9]{1,2}[.][0-9]{1,2}|$package_name-$version|
+s|$package_name-[0-9]{4}[.][0-9]{1,2}[.][0-9]{1,2}|$package_name-$version|
 EOF
 #
 # check_version
